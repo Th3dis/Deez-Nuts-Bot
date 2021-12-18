0ec4ae8acc45ab2502b2169f8a321540c2656721
Th3dis <96215378+Th3dis@users.noreply.github.com>
2021-12-18 16:38:07 -0600

    Create main.py
    Main loop

---
+++ main.py
@@ -0,0 +1,40 @@
+import discord
+import random
+import asyncio
+from discord.ext import commands
+
+token = "OTIxMDk0OTM1NjUyOTkxMDI2.Ybt6gQ.uSbpE-L6gz_HsiYEmkTif2t9fLM"
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Ready for deez nuts!")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if "ligma" in message.content.lower():
+        await message.channel.send("Ligma balls")
+
+    if "sugma" in message.content.lower():
+        await message.channel.send("Sugma balls")
+
+    if "jocelyn" in message.content.lower():
+        await message.channel.send("Jocelyn deez nuts in your mouth")
+
+    if "suck" in message.content.lower():
+        await message.channel.send("Suck on deez nuts")
+
+    if "bofa" in message.content.lower():
+        await message.channel.send("Bofa deez nuts")
+
+client.run(token)
+
+
+
+
+
+
+
